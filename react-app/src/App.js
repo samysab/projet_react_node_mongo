939bdde6c7f749a4fecf0fd07dcab5115d9ac3ea
Théo Sigaud <sigaudtheo@gmail.com>
2022-07-22 16:02:58 +0200

    Add auth router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,21 +9,30 @@ import Profile from "./component/Profile";
 import Confirmation from "./component/Confirmation";
 import Reset from "./component/Reset";
 import Error404 from "./component/404";
+import { AuthProvider } from './component/auth';
+import { RequireAuth } from './component/RequireAuth';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/confirmation">
-            <Route path=":id" element={<Confirmation />} />
-        </Route>
-        <Route path="/resetPassword">
-            <Route path=":id" element={<Reset />} />
-        </Route>
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+      <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+            {/*<Route path="/profile" element={<Profile/>}/>*/}
+            <Route path="/profile" element={
+                <RequireAuth>
+                    <Profile />
+                </RequireAuth>
+            }/>
+            <Route path="/confirmation">
+                <Route path=":id" element={<Confirmation />} />
+            </Route>
+            <Route path="/resetPassword">
+                <Route path=":id" element={<Reset />} />
+            </Route>
+            <Route path="*" element={<Error404 />} />
+          </Routes>
+      </AuthProvider>
 );
 }
 
